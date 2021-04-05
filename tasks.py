73ca3e01e16c644a0ce165ce9b0bee068450c8d6
Kekalainen <git@kekalainen.me>
2021-04-05 03:29:10 +0300

    Add coverage reporting

--- tasks.py
+++ tasks.py
@@ -9,3 +9,13 @@ def start(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src")
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
